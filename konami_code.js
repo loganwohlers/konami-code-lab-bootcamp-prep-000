bfe28a4552d901ba097c9a7519d506ca72d085be
Logan Wohlers <lawohlers@gmail.com>
2019-01-31 17:35:45 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,22 +17,36 @@ const codes = [
 
 function init() {
   var index=0;
+  
   function onKeyDownHandler(e) {
     const key = e.key;
-    if(key === codes[index]){
-      index++;
-      if(index===codes.length){
-        alert("Yay!");
-        index=0;
-      }
-    }else{
-      alert("wrong code!");
-      index=0;
+    
+    if(codes[index] === key) {
+      index++
+    } else {
+      window.alert("Wrong code!")
+      index = 0
     }
+    
+    if(index === codes.length) {
+      window.alert("Yay!")
+      index = 0
+    }
+    
+    // if(key === codes[index]){
+    //   index++;
+    //   if(index===codes.length){
+    //     alert("Yay!");
+    //     index=0;
+    //   }
+    // } else {
+    //   alert("wrong code!");
+    //   index=0;
+    // }
   
   }
-  document.body.addEventListener('keydown', onKeyDownHandler);
   
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
 
 
